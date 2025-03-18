e476088976d7beee80124dd5a248f1daabb1f9c4
Doan Thanh Hieu <hieudoan280102@gmail.com>
2025-03-18 23:20:05 +0700

    sign up BE

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,10 +9,10 @@ dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT;
-
+app.use(express.json);
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
-  console.log("Server is running on PORT: " + PORT );
+  console.log("Server is running on PORT: " + PORT);
   connectDB();
 });

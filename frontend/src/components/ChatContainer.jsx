0ef607d421355aa4c684163427c96896d4c50d9a
Doan Thanh Hieu <hieudoan280102@gmail.com>
2025-04-10 23:20:55 +0700

    Add skeleton to loading

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import ChatHeader from "./ChatHeader";
+import MessageInput from "./MessageInput";
+import { useAuthStore } from "../store/useAuthStore";
+import { useChatStore } from "../store/useChatStore";
+import MessageSkeleton from "./skeletons/MessageSkeleton";
 
 const ChatContainer = () => {
-  return <div>taone may</div>;
+  const {
+    messages,
+    getMessages,
+    isMessagesLoading,
+    selectedUser,
+    subscribeToMessages,
+    unsubscribeFromMessages,
+  } = useChatStore();
+  const { authUser } = useAuthStore();
+
+  if (isMessagesLoading) {
+    return (
+      <div className="flex-1 flex flex-col overflow-auto">
+        <ChatHeader />
+        <MessageSkeleton />
+        <MessageInput />
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex-1 flex flex-col overflow-auto">
+      <ChatHeader />
+      <div>Messages....</div>
+      <MessageInput />
+    </div>
+  );
 };
 
 export default ChatContainer;
